32263ad2fce9557cc081691f4bfb62d01b8ec410
Phillip Yuen <phillip@jomashop.io(opens in new tab)>
2020-07-18 00:12:34 -0400

    Playing with Contexts
    Finished and working example. See https://www.toptal.com/react/react-context-api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import MyProvider from "./MyProvider";
+import ProductList from "./ProductList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    logo = 'google.com/logo.jpg';
+
+    render() {
+        return (
+            <MyProvider>
+                <div className="App">
+                    <header className="App-header">
+                        <h1 className="App-title">Welcome to my web store</h1>
+                    </header>
+                    <ProductList />
+                </div>
+            </MyProvider>
+        );
+    }
 }
 
 export default App;

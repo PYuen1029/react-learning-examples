d5c1458c3219d1605922030d1423d58c1c29aca5
Phillip Yuen <phillip@jomashop.io(opens in new tab)>
2020-07-19 11:02:43 -0400

    Example using reducer
    https://www.robinwieruch.de/react-usereducer-hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import React, {Component} from 'react';
-import MyProvider from "./MyProvider";
-import ProductList from "./ProductList";
+import React from 'react';
 
-class App extends Component {
-    logo = 'google.com/logo.jpg';
+const initialTodos = [
+    {
+        id: 'a',
+        task: 'Learn React',
+        complete: false,
+    },
+    {
+        id: 'b',
+        task: 'Learn Firebase',
+        complete: false,
+    },
+];
 
-    render() {
-        return (
-            <MyProvider>
-                <div className="App">
-                    <header className="App-header">
-                        <h1 className="App-title">Welcome to my web store</h1>
-                    </header>
-                    <ProductList />
-                </div>
-            </MyProvider>
-        );
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        case 'DO_TODO':
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return { ...todo, complete: true };
+                } else {
+                    return todo;
+                }
+            });
+        case 'UNDO_TODO':
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return { ...todo, complete: false };
+                } else {
+                    return todo;
+                }
+            });
+        default:
+            return state;
     }
-}
+};
+
+const App = () => {
+    const [todos, dispatch] = React.useReducer(
+        todoReducer,
+        initialTodos
+    );
+
+    const handleChange = todo => {
+        dispatch({
+            type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+            id: todo.id
+        });
+    };
+
+    return (
+        <ul>
+            {todos.map(todo => (
+                <li key={todo.id}>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={todo.complete}
+                            onChange={() => handleChange(todo)}
+                        />
+                        {todo.task}
+                    </label>
+                </li>
+            ))}
+        </ul>
+    );
+};
 
 export default App;

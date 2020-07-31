3e806832e2ab1a87080efc51fae8cbb9282825f6
Phillip Yuen <phillip@jomashop.io(opens in new tab)>
2020-07-30 22:30:01 -0400

    Contexts:change context provider value in function
    Playing with changing the context value in a somewhat hack-y fashion, in the function, just change context.cars.

--- src/Cars.js
+++ src/Cars.js
@@ -4,20 +4,28 @@ import MyContext from "./MyContext";
 
 const Cars = () => (
     <MyContext.Consumer>
-        {context => (
-            <Fragment>
-                <h4>Cars:</h4>
-                {Object.keys(context.cars).map(carID => (
-                    <Car
-                        key={carID}
-                        name={context.cars[carID].name}
-                        price={context.cars[carID].price}
-                        incrementPrice={() => context.incrementPrice(carID)}
-                        decrementPrice={() => context.decrementPrice(carID)}
-                    />
-                ))}
-            </Fragment>
-        )}
+        {context => {
+            context.cars = {
+                car001: { name: 'Nissan', price: 300 },
+                car002: { name: 'Buick', price: 250 },
+                car003: { name: 'Jeep', price: 300 }
+            }
+
+            return (
+                <Fragment>
+                    <h4>Cars:</h4>
+                    {Object.keys(context.cars).map(carID => (
+                        <Car
+                            key={carID}
+                            name={context.cars[carID].name}
+                            price={context.cars[carID].price}
+                            incrementPrice={() => context.incrementPrice(carID)}
+                            decrementPrice={() => context.decrementPrice(carID)}
+                        />
+                    ))}
+                </Fragment>
+            );
+        }}
     </MyContext.Consumer>
 );
 
